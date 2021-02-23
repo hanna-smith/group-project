0ebf5fa7107a6d398bc5ece941133a4b40ca9415
Hanna Smith <h.m.smith9595@gmail.com>
2021-02-23 14:44:02 -0500

    first commit

---
+++ src/main/java/com/group/Kudos/KudosApplication.java
@@ -0,0 +1,13 @@
+package com.group.Kudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/Kudos/KudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.Kudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
