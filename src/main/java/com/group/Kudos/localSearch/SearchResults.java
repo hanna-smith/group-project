a44e95583d6674697b724f6a17747d9c77165e39
David Carli-Arnold <davocarli@gmail.com>
2021-02-28 10:41:12 -0800

    Add Business model/repo/service & Bing Search capabilities

---
+++ src/main/java/com/group/Kudos/localSearch/SearchResults.java
@@ -0,0 +1,13 @@
+package com.group.Kudos.localSearch;
+
+import java.util.HashMap;
+
+// Container class for search results encapsulates relevant headers and JSON data
+public class SearchResults{
+    HashMap<String, String> relevantHeaders;
+    public String jsonResponse;
+    SearchResults(HashMap<String, String> headers, String json) {
+        relevantHeaders = headers;
+        jsonResponse = json;
+    }
+}
