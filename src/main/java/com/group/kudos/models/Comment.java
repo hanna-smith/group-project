fd176fc9c9e8ad41b565bd769e54e13ba95716fa
David Carli-Arnold <davocarli@gmail.com>
2021-02-28 13:52:55 -0800

    Add Comment model

---
+++ src/main/java/com/group/kudos/models/Comment.java
@@ -0,0 +1,96 @@
+package com.group.kudos.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String comment;
+	
+	@Column(updatable=false)
+	private Date createdAt;
+	private Date updatedAt;
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private Review commentCreator;
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="review_id")
+	private Review reviewComments;
+
+	public Comment() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Review getReviewComments() {
+		return reviewComments;
+	}
+
+	public void setReviewComments(Review reviewComments) {
+		this.reviewComments = reviewComments;
+	}
+	
+	public Review getCommentCreator() {
+		return commentCreator;
+	}
+
+	public void setCommentCreator(Review commentCreator) {
+		this.commentCreator = commentCreator;
+	}
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	@PostPersist
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+}
